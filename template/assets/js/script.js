a8e0ccb78758f09a7ff75eed18d7a7aa8662fbd6
Amias-Burrows <amias.burrows@gmail.com>
2023-02-12 19:34:26 +0000

    Added some more styling and the dark mode toggle JS

--- template/assets/js/script.js
+++ template/assets/js/script.js
@@ -1,28 +1,45 @@
+    // Calls the functions that get called on startup. If not they're not in use or called dynamically whilst the site is running
 const init = () => {
 
     mobile_nav_open();
+    handle_night_mode();
 }
 
+    // Handles opening the nav bar with dragging functionality
 const mobile_nav_open = () => {
     let handle = $('#nav-handle');
     let nav = $('#header-nav');
-    let slider = {
-        start: 0,
-        current: 0,
-        end: 0
-    }
+    // let slider = {
+    //     start: 0,
+    //     current: 0,
+    //     end: 0
+    // }
 
-    handle.mousedown((e) => {
-        slider.start = e.pageX / 100;
-        slider.end = slider.start * 100;
-    })
+    // handle.mousedown((e) => {
+    //     slider.start = e.pageX / 100;
+    //     slider.end = slider.start * 100;
+    // })
 
+        // Simple opening and closing if handle is not pressed and dragged
     handle.click(() => {
         
+            // Flip-Flop for the .open class
         nav.hasClass('open')
             ? nav.removeClass('open')
             : nav.addClass('open');
     })
+}
+
+    // handles the night-mode toggle
+const handle_night_mode = () => {
+    let handle = $('#dark-mode-toggle');
+
+    handle.click(() => {
+        handle.find('.selected')
+            .removeClass('selected')
+            .siblings()
+            .addClass('selected');
+    });
 }
 
 init();
